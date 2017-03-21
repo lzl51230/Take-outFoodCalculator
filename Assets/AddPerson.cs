ca8254e8ae6c9f1170d0ebe469e46364b75d5680
lzl51230 <lzl51230@live.com>
2017-03-21 18:08:31 +0800

    Ver1

---
+++ Assets/AddPerson.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class AddPerson : MonoBehaviour
+{
+    //public Text _result;
+    public List<float> CacheList;
+
+    private EventSystem system;
+    private InputField _input;
+    
+
+    void Start()
+    {
+        system = EventSystem.current;
+        _input = transform.GetComponent<InputField>();
+        CacheList = new List<float>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)) &&
+            system.currentSelectedGameObject == gameObject)
+        {
+            CacheList.Add(float.Parse(_input.text));
+            _input.text = "";
+            _input.ActivateInputField();
+        }
+    }
+
+    public void delete()
+    {
+        if (CacheList.Count > 0)
+        {
+            CacheList.RemoveAt(CacheList.Count - 1);
+        }
+    }
+}
---
+++ Assets/cal.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+public class cal : MonoBehaviour
+{
+    public AddPerson Person, AllPerson;
+    public InputField total;
+
+    private Text Result;
+    private Dictionary<float, float> _calResult = new Dictionary<float, float>(); 
+    // Use this for initialization
+    void Start ()
+    {
+        Result = transform.GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+    private void Update()
+    {
+        //排除所有商品单价总和小于等于0
+        float temp = 0;
+        foreach (var money in Person.CacheList)
+        {
+            temp += money;
+        }
+        if (temp<=0)
+        {
+            temp = 1;
+        }
+
+        //排除当allPersonPrice有值，PersonList无值时的错误
+        float allPersonPrice = 0;
+        foreach (var money in AllPerson.CacheList)
+        {
+            allPersonPrice += money;
+        }
+
+        if (Person.CacheList.Count > 0)
+        {
+            allPersonPrice = allPersonPrice / Person.CacheList.Count;
+        }
+
+        //排除总价小于allPersonPrice的情况
+        float pecent;
+        var totalPrice = float.Parse(total.text);
+        if (totalPrice>0 && totalPrice> allPersonPrice && temp > 0)
+        {
+            pecent = (float.Parse(total.text) - allPersonPrice) / temp;
+        }
+        else
+        {
+            pecent = 1;
+        }
+
+        //计算每个商品
+        _calResult = new Dictionary<float, float>();
+        foreach (var money in Person.CacheList)
+        {
+            var calPrice = money*pecent + allPersonPrice;
+            _calResult.Add(money, calPrice);
+        }
+
+        var uiString = frontMoney();
+        Result.text = uiString;
+    }
+
+
+    private string frontMoney()
+    {
+        var str = new StringBuilder();
+        str.AppendLine("每人外卖价：");
+        foreach (var money in _calResult)
+        {
+            str.AppendLine("原价: "+money.Key.ToString("F2")+"   折后价："+ money.Value.ToString("F2"));
+        }
+
+        str.AppendLine("");
+        str.AppendLine("一起分摊费用：");
+        foreach (var money in AllPerson.CacheList)
+        {
+            str.AppendLine(money.ToString("F2"));
+        }
+
+        return str.ToString();
+    }
+}
